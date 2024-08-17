63fd4371105a210b697adaac9289d752692fb370
Sibtain Asad <sibtainasad50@gmail.com>
2024-08-18 03:15:28 +0500

    update: avatar - hide for mobile

--- src/components/sections/Avatar.tsx
+++ src/components/sections/Avatar.tsx
@@ -3,11 +3,20 @@ import { avatar } from '../../assets';
 
 const Avatar = () => {
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, ease: 'easeInOut', delay: 1 }} className="relative">
-    <div className="relative bottom-0 left-52">
-      <img src={avatar} alt="Avatar" className="rounded-full border-4 border-slate-800 w-96 h-96" />
-    </div>
-  </motion.div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeInOut', delay: 1 }}
+      className="relative"
+    >
+      <div className="relative bottom-0 hidden md:block left-52">
+        <img
+          src={avatar}
+          alt="Avatar"
+          className="rounded-full border-4 border-slate-800 w-96 h-96"
+        />
+      </div>
+    </motion.div>
   );
 };
 
