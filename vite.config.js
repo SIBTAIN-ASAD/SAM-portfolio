43fe401760200018f9ac3aa38b746c3d19a75d6d
SIBTAIN-ASAD <sibtainasad50@gmail.com>
2025-03-06 15:48:05 +0500

    update: vite - config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext", // Ensure modern syntax including top-level await is supported
+    minify: "esbuild", // Optional: Keep esbuild minifier, or set to false for debugging
+  },
+  esbuild: {
+    supported: {
+      "top-level-await": true, // Explicitly enable top-level await
+    },
+  },
 });
